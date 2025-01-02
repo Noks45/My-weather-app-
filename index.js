578d119a8d739d20077a5264c2d7329df838b6d4
Nokuthula Khumalo <noksmbulase@gmail.com>
2025-01-02 12:23:49 +0200

    Kept the original API key

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "c6691aoaf052f4b37d57tbbee09540c5";
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
